9f8753a4c7ed826767e0856a9d98a708056e33c7
Damir Zhumatayev <adidukik@gmail.com>
2023-12-05 02:27:34 +0900

    Rewrite ButtonGroupRightPanel in functional style

--- app/components/ButtonGroupRightPanel/ButtonGroupRightPanel.tsx
+++ app/components/ButtonGroupRightPanel/ButtonGroupRightPanel.tsx
@@ -1,23 +1,19 @@
 import React from 'react';
 import { Button } from '@nextui-org/react';
 
+const buttonLabels = ['Input', 'Output', 'Instruct', 'Semantic Search'];
+
 const ButtonGroupRightPanel = () => {
   return (
     <div className="grid grid-cols-1 gap-0">
-      <Button className="w-full rounded-none border border-gray bg-white">
-        <span className="text-left w-full block">Input</span>
-      </Button>
-      <Button className="w-full rounded-none border border-gray bg-white">
-        <span className="text-left w-full block">Output</span>
-      </Button>
-      <Button className="w-full rounded-none border border-gray bg-white">
-        <span className="text-left w-full block">Instruct</span>
-      </Button>
-      <Button className="w-full rounded-none border border-gray bg-white">
-        <span className="text-left w-full block">Semantic Search</span>
-      </Button>
+      {buttonLabels.map((label) => (
+        <Button key={label} className="w-full rounded-none border border-gray bg-white">
+          <span className="text-left w-full block">{label}</span>
+        </Button>
+      ))}
     </div>
   );
 };
+
 
 export default ButtonGroupRightPanel;
